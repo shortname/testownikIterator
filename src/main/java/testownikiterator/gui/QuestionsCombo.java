ffbad35236120f077381ea48ff267bc181d46c6d
Kuba Dymon <dymonjakub@gmail.com>
2017-06-21 16:43:40 +0200

    Lambda zamiast interfejsu

--- src/main/java/testownikiterator/gui/QuestionsCombo.java
+++ src/main/java/testownikiterator/gui/QuestionsCombo.java
@@ -3,7 +3,6 @@ package testownikiterator.gui;
 import testownikiterator.data.Item;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ItemListener;
 import java.util.List;
 
@@ -12,11 +11,6 @@ public class QuestionsCombo extends JComboBox {
     public QuestionsCombo(List<Item> items, ItemListener itemListener) {
         items.stream().map(Item::toLabel).forEach(this::addItem);
         addItemListener(itemListener);
-        setRenderer(new ListCellRenderer() {
-            @Override
-            public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-                return (JLabel) value;
-            }
-        });
+        setRenderer((list, value, index, isSelected, cellHasFocus) -> (JLabel) value);
     }
 }
